063d5a446128c14b87b9ca3d36228abe59feb9b0
XieFei <xuejingwen25@163.com>
2019-01-07 16:35:57 +0800

    创建微信API 代理 组件化工程

---
+++ WeChatApiManager/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  WeChatApiManager
+//
+//  Created by YanYi on 2019/1/7.
+//  Copyright © 2019 YanYi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UILabel *lab = [[UILabel alloc]init];
+    lab.frame = self.view.frame;
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.text = @"WeChatApiManager";
+    lab.font = [UIFont systemFontOfSize:28];
+    lab.textColor = [UIColor greenColor];
+    [self.view addSubview:lab];
+    lab.center = self.view.center;
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
